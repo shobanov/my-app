df2a2cbe01e08c92e55882d8dd519d46108019fc
shobanov <shobanoff@gmail.com>
2020-11-26 15:26:26 +0300

    add-test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Technologies from './Technologies';
+
+const App = () => {
+  return (
+    <div>
+      <Footer />
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <a href="#s">Home</a> -
+      <a href="#s">News Feed</a> -
+      <a href="#s">Message</a>
+    </div>
+  );
+}
+
+  export default Header;
