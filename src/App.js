3281b2eb546313ca0711d9c99f5ec2c764052ba2
shobanov <shobanoff@gmail.com>
2020-11-28 18:44:05 +0300

    add css modules

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMZ-RdJiAhq6BYXAQfCrrv2VrO9b8xJgxtfw&usqp=CAU'/>
-      </header>
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-        <div className='content'>
-          <img src='https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg'/>
-        </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return  <header className={s.header}>
+              <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMZ-RdJiAhq6BYXAQfCrrv2VrO9b8xJgxtfw&usqp=CAU'/>
+            </header>;
+}
+
+export default Header;
