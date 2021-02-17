d7058de16cf5b9b60c528a0f57164c7ce7163e52
shobanov <shobanoff@gmail.com>
2021-02-17 21:51:28 +0300

    add store + getState

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {subscribe} from './redux/state';
-import {addPost, updateNewPostText} from './redux/state';
+import store from './redux/state';
 import {BrowserRouter} from 'react-router-dom';
 
-export let rerenderEntireTree = (state) => { // ф-ция переотресовки дерева при изменении state для его актуальности
-  ReactDOM.render(
+export let rerenderEntireTree = () => { // ф-ция переотресовки дерева при изменении state для его актуальности
+  ReactDOM.render (
     <BrowserRouter>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={store.getState()}
+           addPost={store.addPost.bind(store)} // связывем, что бы владелец метода сохранился, т.к. .this ссылается на _state
+           updateNewPostText={store.updateNewPostText.bind(store)}/>
     </BrowserRouter>,
     document.getElementById('root')); // точка входа приложения (обращение к index.html)
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
 /* 
 Вызываем ф-цию из того мира, и передаём ей то что мы хотим отдать тому миру.
@@ -23,6 +24,6 @@ rerenderEntireTree(state);
 Это и есть колбэк (мы отдаём ф-цию, что бы кто-то её вызвал)
 */
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 reportWebVitals(); // no name
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,60 @@
-  // Добиваемся того, что-бы в state из index ничего не импортировать. 
-  
-  let getRerender = () => { //2) Нашли, заходим в subscribe (потому что можем блять и всё тут)
-     
-  }
+// Добиваемся того, что-бы в store из index ничего не импортировать.
+// Все функции и все переменные упаковываем в один объект store
 
-let state = {
-  
-  profilePage: {
-    posts: [
-      {id: 1, message: 'Hi, how are you?', likesCount: 12},
-      {id: 2, message: 'Runs the app in the development mode', likesCount: 32}
-    ],
-    newPostText: ''
+let store = {  
+  _state: {  // является приватным свойством
+    profilePage: {
+      posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'Runs the app in the development mode', likesCount: 32}
+      ],
+      newPostText: ''
+    },
+
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Aleksey' },
+        { id: 2, name: 'Anna' }
+      ],
+      messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How are you?' }
+      ]
+    }
+  },
+
+  getState() {  // гетер, задача кот. возвр. _state(т.к. к нему нельзя обращ. на прямую)
+    return this._state;  // к св-ву или методу объекта нужно обр. через ключ. слово .this (т.к. имя пер. может меняться)
+  },
+
+  _getRerender () {  //2) Нашли, заходим в subscribe (потому что можем блять и всё тут) | это теперь метод объекта store
+
   },
   
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Aleksey' },
-      { id: 2, name: 'Anna' }
-    ],
-    
-    messages: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'How are you?' }
-    ]
+  addPost () {  //теперь ты метод
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+    this._state.profilePage.posts.push (newPost);
+    this._state.profilePage.newPostText = '';
+    this._state._getRerender(this._state);     //1) тут происходит замыкание, мы выпрыгиваем вверх и ищем эту ф-цию 
+  },
+
+  updateNewPostText (userText) {  // userText(имя параметра важно только тут) приходит из ф-ции onPostChange(MyPosts.jsx)
+    this._state.profilePage.newPostText = userText;
+    this._state._getRerender (this._state); //1)
+  },
+
+  subscribe (observer) {  // в subscribe сидит пар-р rerenderEntireTree
+    this._state._getRerender = observer; //3) всё, пиздец, придётся переотрисовать
   }
 }
-
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-  state.profilePage.posts.push (newPost);
-  state.profilePage.newPostText = '';
-  getRerender(state); //1) тут происходит замыкание, мы выпрыгиваем вверх и ищем эту ф-цию 
-}
-
-export const updateNewPostText = (userText) => {  // userText(название параметра важно только тут) приходит из ф-ции onPostChange
-  state.profilePage.newPostText = userText;
-  getRerender(state); //1)
-}
+  
 /*
 subscribe может вызвать тот кто её импортирует и передать внутрь другую функцию.
 это кстати паттерн - "observer"
 */
-export const subscribe = (observer) => { // в subscribe сидит параметр rerenderEntireTree
-  getRerender = observer; //3) всё, пиздец, придётся переотрисовать
-}
 
-export default state;
+export default store;
